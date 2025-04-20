f23f0eb458d16fbfd587988b3033cd0a0406e6f7
Saurabh Kumar <skfateha@gmail.com>
2025-04-20 15:57:20 +0530

    test cases added for existing system

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,61 @@
-# -*- coding: utf-8 -*-
-import unittest
+import pytest
 
-from gilded_rose import Item, GildedRose
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
 
+    def __repr__(self):
+        return f"{self.name}, {self.sell_in}, {self.quality}"
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+from gilded_rose import GildedRose
 
-        
-if __name__ == '__main__':
-    unittest.main()
+def update_and_return(name, sell_in, quality):
+    items = [Item(name, sell_in, quality)]
+    inv = GildedRose(items)
+    inv.update_quality()
+    return items[0]
+
+def test_standard_item_degradation():
+    item = update_and_return("Standard Item", 10, 20)
+    assert item.sell_in == 9
+    assert item.quality == 19
+
+def test_quality_degrades_twice_after_sell_date():
+    item = update_and_return("Standard Item", 0, 10)
+    assert item.sell_in == -1
+    assert item.quality == 8
+
+def test_quality_never_negative():
+    item = update_and_return("Standard Item", 5, 0)
+    assert item.quality == 0
+
+def test_aged_brie_increases_quality():
+    item = update_and_return("Aged Brie", 2, 0)
+    assert item.quality == 1
+
+def test_aged_brie_max_quality_50():
+    item = update_and_return("Aged Brie", 2, 50)
+    assert item.quality == 50
+
+def test_sulfuras_constant():
+    item = update_and_return("Sulfuras, Hand of Ragnaros", 0, 80)
+    assert item.sell_in == 0
+    assert item.quality == 80
+
+def test_backstage_increase_by_1():
+    item = update_and_return("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    assert item.quality == 21
+
+def test_backstage_increase_by_2():
+    item = update_and_return("Backstage passes to a TAFKAL80ETC concert", 10, 20)
+    assert item.quality == 22
+
+def test_backstage_increase_by_3():
+    item = update_and_return("Backstage passes to a TAFKAL80ETC concert", 5, 20)
+    assert item.quality == 23
+
+def test_backstage_drops_to_zero():
+    item = update_and_return("Backstage passes to a TAFKAL80ETC concert", 0, 20)
+    assert item.quality == 0
